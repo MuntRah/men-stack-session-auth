a1b8edafbdf7199603e6c21a52bb2c80548a3d47
MuntRah <me.alrahim.02@gmail.com>
2024-07-23 11:02:31 +0300

    add the sign-up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-router.get('sign-up' , (req , res , next)=>{
-  res.render('sign-up.ejs')
-})
+router.get("/sign-up", (req, res) => {
+  res.render("auth/sign-up.ejs");
+});
 module.exports = router;
 
