bf50f26922f6b53edd93b87aeed3d4f8a2299dc4
MuntRah <me.alrahim.02@gmail.com>
2024-07-23 09:51:05 +0300

    setup the poilerblate

---
+++ condig/database.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL);
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB' ${mongoose.connection.name}`);
+});
